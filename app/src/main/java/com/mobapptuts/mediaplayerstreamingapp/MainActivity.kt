229f08be02a97d9322cf3a6c5177e64cd4f37b4c
Nigel Henshaw <nigel@mobapptuts.com>
2017-11-12 13:02:27 +0900

    Working implementation of mediaplayer stream url with SurfaceView

---
+++ app/src/main/java/com/mobapptuts/mediaplayerstreamingapp/MainActivity.kt
@@ -0,0 +1,107 @@
+package com.mobapptuts.mediaplayerstreamingapp
+
+import android.graphics.Point
+import android.media.AudioAttributes
+import android.media.MediaPlayer
+import android.net.Uri
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Surface
+import android.view.SurfaceHolder
+import android.view.View
+import android.widget.FrameLayout
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity(), SurfaceHolder.Callback {
+
+    override fun surfaceChanged(p0: SurfaceHolder?, p1: Int, p2: Int, p3: Int) {
+    }
+
+    override fun surfaceDestroyed(p0: SurfaceHolder?) {
+    }
+
+    override fun surfaceCreated(holder: SurfaceHolder) {
+        val surface = holder.surface
+        setupMediaPlayer(surface)
+        prepareMediaPlayer()
+    }
+
+    private lateinit var mediaPlayer: MediaPlayer
+    private var playbackPosition = 0
+    private val rtspUrl = "rtsp://184.72.239.149/vod/mp4:BigBuckBunny_175k.mov"
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val holder = surfaceView.holder
+        holder.addCallback(this)
+    }
+
+    override fun onPause() {
+        super.onPause()
+
+        playbackPosition = mediaPlayer.currentPosition
+    }
+
+    override fun onStop() {
+        mediaPlayer.stop()
+        mediaPlayer.release()
+
+        super.onStop()
+    }
+
+    private fun createAudioAttributes(): AudioAttributes {
+        val builder = AudioAttributes.Builder()
+                .setUsage(AudioAttributes.USAGE_MEDIA)
+                .setContentType(AudioAttributes.CONTENT_TYPE_MOVIE)
+        return builder.build()
+    }
+
+    private fun setupMediaPlayer(surface: Surface) {
+        progressBar.visibility = View.VISIBLE
+        mediaPlayer = MediaPlayer()
+        mediaPlayer.setSurface(surface)
+        val audioAttributes = createAudioAttributes()
+        mediaPlayer.setAudioAttributes(audioAttributes)
+        val uri = Uri.parse(rtspUrl)
+        try {
+            mediaPlayer.setDataSource(this, uri)
+        } catch (e: IllegalArgumentException) {
+            e.printStackTrace()
+        }
+    }
+
+    private fun setSurfaceDimensions(player: MediaPlayer, width: Int, height: Int) {
+        if(width > 0 && height > 0) {
+            val aspectRatio = height.toFloat() / width.toFloat()
+            val screenDimensions = Point()
+            windowManager.defaultDisplay.getSize(screenDimensions)
+            val surfaceWidth = screenDimensions.x
+            val surfaceHeight = (surfaceWidth * aspectRatio).toInt()
+            val params = FrameLayout.LayoutParams(surfaceWidth, surfaceHeight)
+            surfaceView.layoutParams = params
+            val holder = surfaceView.holder
+            player.setDisplay(holder)
+        }
+    }
+
+    private fun prepareMediaPlayer() {
+        try {
+            mediaPlayer.prepareAsync()
+        } catch (e: IllegalStateException) {
+            e.printStackTrace()
+        }
+
+        mediaPlayer.setOnPreparedListener {
+            progressBar.visibility = View.INVISIBLE
+            mediaPlayer.seekTo(playbackPosition)
+            mediaPlayer.start()
+        }
+
+        mediaPlayer.setOnVideoSizeChangedListener { player, width, height ->
+            setSurfaceDimensions(player, width, height)
+        }
+    }
+
+}
